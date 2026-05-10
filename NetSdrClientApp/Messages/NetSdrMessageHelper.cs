6965a95c6efe57c8b8ebcaaa827a8246fe129f5b
VlasenkoMykola <vlasenko.mykola.sh264@gmail.com>
2026-05-10 22:28:34 +0300

    lab 5 green commit

--- NetSdrClientApp/Messages/NetSdrMessageHelper.cs
+++ NetSdrClientApp/Messages/NetSdrMessageHelper.cs
@@ -4,16 +4,12 @@ using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
-using NetSdrClientApp.Networking;
 
 namespace NetSdrClientApp.Messages
 {
     //TODO: analyze possible use of [StructLayout] for better performance and readability 
     public static class NetSdrMessageHelper
     {
-        // Violating architecture: Messages should not depend on Networking
-        public static Type GetDefaultClientType() => typeof(ITcpClient);
-
         private const short _maxMessageLength = 8191;
         private const short _maxDataItemMessageLength = 8194;
         private const short _msgHeaderLength = 2; //2 byte, 16 bit
